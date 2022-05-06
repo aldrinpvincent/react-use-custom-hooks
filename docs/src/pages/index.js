ee1bb514ebd9d21d090156a5dd7ce7a90f8dd57a
aldrinpvincent <aldrinpvincent@gmail.com>
2022-05-06 13:48:40 +0530

    updated CTA path

--- docs/src/pages/index.js
+++ docs/src/pages/index.js
@@ -16,7 +16,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--lg"
-            to="/docs/useDebounce"
+            to="/docs/useAsync"
           >
             View Hooks
           </Link>
